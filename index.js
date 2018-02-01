0a94e2ebb5d276393060d7d7b490bb9705c64fed
Brendon Earl <bearl@uvic.ca>
2018-02-01 13:53:06 -0800

    init mqtt client

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 require("./api/routes")(app);
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://test.mosquitto.org");
+require("./api/mqttclient")(client);
