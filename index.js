7294f82eefe36193e5ee2c441cb58e66c44b9a96
Brendon Earl <bearl@uvic.ca>
2018-02-24 19:39:56 -0800

    remove index on-the-fly test code

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+process.on("unhandledRejection", r => console.log(r));
+
+const data_store = require("./storage/service").open_connection();
+
+// setTimeout(() => {
 const app = require("./express/service")();
 
 require("./socket/service")(app);
 
 require("./mqtt/service")();
-
-const data_store = require("./storage/service").open_connection();
+// }, 1000);
