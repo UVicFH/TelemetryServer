e4174987bb0d2136a5b56887243981c8f3f5167d
Brendon Earl <bearl@uvic.ca>
2018-02-24 19:40:31 -0800

    refactor to arrow functions and integrate data storage to mqtt receiving

--- logic/handle_mqtt.js
+++ logic/handle_mqtt.js
@@ -1,13 +1,16 @@
+const data_store_actions = require("../storage/actions");
+
 module.exports = {
-  handle_connect: function(client) {
+  handle_connect: client => {
     client.subscribe("presence");
     client.publish("presence", "Hello mqtt");
   },
-  handle_message: function(client, { topic, message }) {
+  handle_message: (client, { topic, message }) => {
     // message is Buffer
     console.log("handling message");
     console.log(topic.toString());
     console.log(message.toString());
+    data_store_actions.write_data({ msg: message.toString() });
     client.end();
   }
 };
