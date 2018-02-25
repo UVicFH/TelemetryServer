f1ab3ead2cda64a4feea7cfda4a146f3af172ef8
Brendon Earl <bearl@uvic.ca>
2018-02-24 19:42:12 -0800

    refactor to async await & fix async data store connection bug

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ process.on("unhandledRejection", r => console.log(r));
 
 const data_store = require("./storage/service").open_connection();
 
-// setTimeout(() => {
 const app = require("./express/service")();
 
 require("./socket/service")(app);
 
 require("./mqtt/service")();
-// }, 1000);
--- storage/actions.js
+++ storage/actions.js
@@ -1,44 +1,71 @@
 const data_store = require("./service");
 const assert = require("assert");
 
-const test_insert_docs = (data, callback = () => {}) => {
-  // Insert some documents
-  data_store.insert_data([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
-    assert.equal(err, null);
+const test_insert_docs = async data => {
+  let result;
+
+  // Insert some test objects
+  try {
+    result = await data_store.collection.insertMany([
+      { a: 1 },
+      { a: 2 },
+      { a: 3 }
+    ]);
+  } catch (error) {
+    console.error(`Failed inserting test data ${data}`);
+  }
+
+  // test objects where inserted
+  try {
     assert.equal(3, result.result.n);
     assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
+  } catch (error) {
+    console.error(`Failed test assertion ${data}`);
+    throw error;
+  }
+
+  console.log("Inserted 3 documents into the collection");
+  return result;
 };
 
 /**
  * Write data to database
- * @param {Array | *} data
- * @callback {inser_callback} callback - callback called after data has been inserted (or failed to)
+ * @param {Array | *} data - data or datum to be added to the data store
  */
-const write_data = (data, callback = () => {}) => {
-  // Use appropraite insert depending on data type
-  if (Array.isArray(data)) data_store.insert_data(data, callback);
-  else data_store.insert_datum(data, callback);
+const write_data = async data => {
+  let result;
+
+  try {
+    // Use appropraite insert depending on data type
+    result = Array.isArray(data)
+      ? await data_store.collection.insertMany(data)
+      : await data_store.collection.insert(data);
+  } catch (error) {
+    console.error(`Failed inserting data ${data}`);
+    throw error;
+  }
 
   // assert insertion was successful
-  assert.equal(err, null);
-  if (Array.isArray(data)) {
-    assert.equal(data.length, result.result.n);
-    assert.equal(data.length, result.ops.length);
-  } else {
-    assert.equal(1, result.result.n);
-    assert.equal(1, result.ops.length);
+  try {
+    if (Array.isArray(data)) {
+      assert.equal(data.length, await esult.result.n);
+      assert.equal(data.length, await result.ops.length);
+    } else {
+      assert.equal(1, await result.result.n);
+      assert.equal(1, await result.ops.length);
+    }
+  } catch (error) {
+    console.error(`Failed assertion: ${error}`);
+    throw error;
   }
 
   // Notify user insertion sucessful
   console.log(`Inserted ${result.result.n} documents into the collection`);
-  callback(result);
+  return result;
 };
 
 module.exports = {
   write_data
 };
 
-test_insert_docs();
+setTimeout(() => test_insert_docs(), 2000);
