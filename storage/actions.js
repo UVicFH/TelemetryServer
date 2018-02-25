acb6350faa70bac4a89b0067473b24fbc5ce1ff3
Brendon Earl <bearl@uvic.ca>
2018-02-24 19:46:05 -0800

    test insert doc now uses write_data method instead of re-implementing

--- storage/actions.js
+++ storage/actions.js
@@ -1,33 +1,6 @@
 const data_store = require("./service");
 const assert = require("assert");
 
-const test_insert_docs = async data => {
-  let result;
-
-  // Insert some test objects
-  try {
-    result = await data_store.collection.insertMany([
-      { a: 1 },
-      { a: 2 },
-      { a: 3 }
-    ]);
-  } catch (error) {
-    console.error(`Failed inserting test data ${data}`);
-  }
-
-  // test objects where inserted
-  try {
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-  } catch (error) {
-    console.error(`Failed test assertion ${data}`);
-    throw error;
-  }
-
-  console.log("Inserted 3 documents into the collection");
-  return result;
-};
-
 /**
  * Write data to database
  * @param {Array | *} data - data or datum to be added to the data store
@@ -48,7 +21,7 @@ const write_data = async data => {
   // assert insertion was successful
   try {
     if (Array.isArray(data)) {
-      assert.equal(data.length, await esult.result.n);
+      assert.equal(data.length, await result.result.n);
       assert.equal(data.length, await result.ops.length);
     } else {
       assert.equal(1, await result.result.n);
@@ -66,6 +39,10 @@ const write_data = async data => {
 
 module.exports = {
   write_data
+};
+
+const test_insert_docs = async () => {
+  write_data([{ a: 1 }, { a: 2 }, { a: 3 }]);
 };
 
 setTimeout(() => test_insert_docs(), 2000);
