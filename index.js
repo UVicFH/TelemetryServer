d71b163ef5a6993ef29ef53403ec758cc2ef01dc
Brendon Earl <bearl@uvic.ca>
2018-04-26 20:50:55 -0700

    Double to single quotes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // force better logging
-process.on("unhandledRejection", r => console.log(r));
+process.on('unhandledRejection', r => console.log(r));
 
-const data_store = require("./storage/service").open_connection();
+const data_store = require('./storage/service').open_connection();
 
-require("./express/service").init();
+require('./express/service').init();
 
-const express_app = require("./express/service").get_service();
+const express_app = require('./express/service').get_service();
 
-require("./socket/service").init(express_app);
-require("./mqtt/service").init();
+require('./socket/service').init(express_app);
+require('./mqtt/service').init();
 
-require("./socket/api").activate();
-require("./mqtt/api").activate();
+require('./socket/api').activate();
+require('./mqtt/api').activate();
--- logic/handle_mqtt.js
+++ logic/handle_mqtt.js
@@ -1,18 +1,21 @@
-const data_store_actions = require("../storage/actions");
-const socket_actions = require("../socket/actions");
+const data_store_actions = require('../storage/actions');
+const socket_actions = require('../socket/actions');
 
 module.exports = {
   handle_connect: client => {
-    client.subscribe("hybrid/#");
-    client.publish("presence", "Hello mqtt");
+    client.subscribe('hybrid/#');
+    client.publish('presence', 'Hello mqtt');
   },
   handle_message: (client, { topic, message }) => {
     // message is Buffer
-    console.log("handling mqtt message");
-    console.log(`topic: ${topic.toString()}`);
-    console.log(`msg: ${message.toString()}`);
+    console.info(
+      `handling mqtt message\n` +
+        `topic: ${topic.toString()}\n` +
+        `msg: ${message.toString()}\n`
+    );
 
     data_store_actions.write_data({ msg: message.toString() });
+    // TODO: change msg & document why
     socket_actions.send_data({ msg: message.toString() });
     client.end();
   }
