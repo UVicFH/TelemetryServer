bf0ff57c80470a171b451c6a0cddd406fff64499
Brendon Earl <bearl@uvic.ca>
2018-05-10 23:22:33 -0700

    Add sending record and logic for signal staggering

--- mqtt/mqttHandler.js
+++ mqtt/mqttHandler.js
@@ -4,6 +4,8 @@
 
 const data_store_actions = require(__base + '/storage/storageActions');
 const socket_actions = require(__base + '/socket/socketActions');
+
+const lastSentRecords = {};
 const TIMEOUT = 500;
 
 module.exports = {
@@ -21,8 +23,21 @@ module.exports = {
 
     data_store_actions.write_data({ msg: message.toString() });
     // TODO: change msg & document why
-    conole.log(Object.keys(message));
-    socket_actions.send_data({ msg: message.toString() });
+    // object for messages which haven't been sent for greater than TIMEOUT
+    const prunedMessage = {};
+    const now = Date.now();
+    // prune message signals
+    Object.keys(message).forEach(signalKey => {
+      if (now - lastSentRecords[key] > TIMEOUT) {
+        // add message signal
+        prunedMessage[key] = message[key];
+        // reset 'last sent time'
+        lastSentRecords[key] = now;
+      }
+    });
+    if (Object.keys(prunedMessage !== 0)) {
+      socket_actions.send_data({ msg: prunedMessage.toString() });
+    }
     client.end();
   }
 };
