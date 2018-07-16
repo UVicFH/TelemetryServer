962d5af1c8e2469e78ccd6b1fa85b11b887336c0
Brendon Earl <bearl@uvic.ca>
2018-07-15 21:02:08 -0700

    simplify top level index

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // force better logging
 process.on('unhandledRejection', r => console.log(r));
 
-// create global for module path resolution
-global.__base = __dirname;
-
 /** The following commands are to initialize the service & activate their api */
-const data_store = require('./storage').service.open_connection();
+require('./storage').service.open_connection();
 
 require('./web/http').service;
 require('./web/http').api.activate();
