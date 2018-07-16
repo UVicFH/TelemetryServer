b4ea0ceba675b6017f71efdbcb41b73cea08d845
Brendon Earl <bearl@uvic.ca>
2018-07-15 21:59:33 -0700

    Run main in async function to correct storage init timing

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 process.on('unhandledRejection', r => console.log(r));
 
 /** The following commands are to initialize the service & activate their api */
-require('./storage').service.open_connection();
+async function main() {
+    await require('./storage').service.open_connection();
 
-require('./web/http').service;
-require('./web/http').api.activate();
+    require('./web/http').service;
+    require('./web/http').api.activate();
 
-require('./web/socket').service;
-require('./web/socket').api.activate();
+    require('./web/socket').service;
+    require('./web/socket').api.activate();
 
-require('./mqtt').service;
-require('./mqtt').api.activate();
+    require('./mqtt').service;
+    require('./mqtt').api.activate();
+};
+
+main();
