647e9daf3caf3561f37e83404f1986b51e1cf96e
Jayden Chan <jaydencn7@gmail.com>
2019-05-30 20:23:25 -0700

    JC: Delete old server

--- web/socket/index.js
+++
@@ -1,9 +0,0 @@
-const actions = require('./socketActions');
-const api = require('./socketApi');
-const service = require('./socketService');
-
-module.exports = {
-  actions,
-  api,
-  service,
-};
