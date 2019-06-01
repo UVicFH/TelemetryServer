1dc8a4e616473c79f994b5a9916ff7c59ded4f60
Jayden Chan <jaydencn7@gmail.com>
2019-06-01 16:34:46 -0700

    JC: (WIP) Begin implementing the socket stuff

--- src/modules/net/index.ts
+++ src/modules/net/index.ts
@@ -6,4 +6,32 @@
  * @author Chad McColm
  */
 
+import { join } from 'path';
+import { createServer } from 'http';
 import * as express from 'express';
+
+import { getLogger } from '../logger';
+import * as socket from './socket';
+
+const logger = getLogger('net');
+
+export function run() {
+  logger.info('Initializing Express/HTTP/socket.io');
+  let expressApp, io;
+
+  try {
+    expressApp = express();
+    const httpServer = createServer(expressApp);
+    socket.init(httpServer);
+  } catch (e) {
+    logger.error('Failed to initialize network module');
+    logger.error(e);
+
+    throw e;
+  }
+
+  logger.info('Successfully initialized Express/HTTP/socket.io');
+
+  expressApp.use(express.static(join(__dirname, '/public')));
+  logger.info('Express configured to serve static /public folder');
+}
